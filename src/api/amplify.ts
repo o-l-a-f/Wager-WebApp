6b5d2cd394c5048b6369a2141afcb3d147ce3f8e
Olaf S <50432051+o-l-a-f@users.noreply.github.com>
2023-11-11 00:32:52 -0800

    Update amplify.ts
    update appconfig key

--- src/api/amplify.ts
+++ src/api/amplify.ts
@@ -6,7 +6,7 @@ export class AmplifyConfiguration {
             aws_appsync_region: "us-east-1",
             aws_appsync_graphqlEndpoint: "https://xrqpwgbe2nbxjcpaayn7rafefu.appsync-api.us-east-1.amazonaws.com/graphql",
             aws_appsync_authenticationType: "API_KEY",
-            aws_appsync_apiKey: "da2-2lpk5z4lprbx5dt3x56mqszxxi"
+            aws_appsync_apiKey: "da2-xb6qgipwpfbatk6xqenbkfpjqa"
         })
     }
 }
